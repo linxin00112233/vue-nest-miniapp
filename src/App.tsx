78649d90ccfc903cfee4dc3099313fde34b5916d
安安 <linxin00112233@126.com>
2025-03-13 01:11:46 +0800

    Changes

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import {useState, useRef, RefObject} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState<number>(0)
+    const handleClick = (val: number): void => {
+        console.log('点击了')
+        console.log('11')
+        setCount(count + val)
+    }
+    const hello: RefObject<string> = useRef<string>('hello world')
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => handleClick(2)}>
+                    count is {count} {hello.current}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
+
+export default App
